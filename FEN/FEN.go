5de1becf0f45fb8bf26e5e3ff1b2f19e5a20d167
CasualBrainUtilisation <120629434+CasualBrainUtilisation@users.noreply.github.com>
2023-04-17 15:14:22 +0100

    created LoadPositionFromFEN

--- FEN/FEN.go
+++ FEN/FEN.go
@@ -1,1 +1,9 @@
 package FEN //File that manages loading positions out of a FEN-string, or saving positions to a FEN-string
+import "github.com/CasualBrainUtilisation/GoChess/Pieces"
+
+func LoadPositionFromFEN(fen string) []Pieces.Piece {
+
+	var toReturn []Pieces.Piece = make([]Pieces.Piece, 0, 0) //create list with ChessPieces, representing a chess position, that'll later be returned
+
+	return toReturn //return created list
+}
