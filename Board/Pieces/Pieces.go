54db739dbb3424f82835c55fe0af69770c9861dd
CasualBrainUtilisation <120629434+CasualBrainUtilisation@users.noreply.github.com>
2023-04-17 14:59:14 +0100

    added PieceType + PieceColor enums

--- Board/Pieces/Pieces.go
+++ Board/Pieces/Pieces.go
@@ -1,15 +1,26 @@
 package Pieces
 
+//enums (or the og version of them)
+//enum that represents the chessPiece.Type
 type PieceType int //type used to represent the type of a chessPiece
 
-const (
-	Pawn   = 0
-	Knight = 1
-	Bishop = 2
-	Rock   = 3
-	Queen  = 4
-	King   = 5
+const ( //defines all PieceTypes to be used as enum values
+	Pawn   PieceType = 0
+	Knight PieceType = 1
+	Bishop PieceType = 2
+	Rock   PieceType = 3
+	Queen  PieceType = 4
+	King   PieceType = 5
+)
+
+//enum that represents the chessPiece.color
+type PieceColor int //type used to represent the color of a chessPiece
+
+const ( //defines all PieceColors to be used as enum values
+	White PieceColor = 0
+	Black PieceColor = 1
 )
 
 type Piece struct { //struct that represents any chessPiece from pawn to king
+	pieceType PieceType //variable representing the type of the piece for example: 1 for Knight, those are definied in the consts() above
 }
