5df32521f85119a1e15a6960ef360f2ba950f12e
CasualBrainUtilisation <120629434+CasualBrainUtilisation@users.noreply.github.com>
2023-04-17 15:08:31 +0100

    added print to use piece

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main //this is the main file, meaning it'll handle all the other packages, which theirself will handle most chess game logic
 
 import (
+	"fmt"
+
 	"github.com/CasualBrainUtilisation/GoChess/Fields"
 	"github.com/CasualBrainUtilisation/GoChess/Pieces"
 )
 
 func main() {
 	var piece Pieces.Piece = Pieces.Piece{Pieces.Bishop, Pieces.Black, Fields.BoardField{1, 2}}
+
+	fmt.Println(piece)
 }
