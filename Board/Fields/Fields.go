6f2d23e308596346d391b7a8de948c167f39eb0f
CasualBrainUtilisation <120629434+CasualBrainUtilisation@users.noreply.github.com>
2023-04-17 14:54:07 +0100

    added Fields and moved Pieces

---
+++ Board/Fields/Fields.go
@@ -0,0 +1,4 @@
+package Fields
+
+type BoardField struct { //struct representing any field on the chessBoard, used primarly to represent a piece's position
+}
---
+++ Board/Pieces/Pieces.go
@@ -0,0 +1,15 @@
+package Pieces
+
+type PieceType int //type used to represent the type of a chessPiece
+
+const (
+	Pawn   = 0
+	Knight = 1
+	Bishop = 2
+	Rock   = 3
+	Queen  = 4
+	King   = 5
+)
+
+type Piece struct { //struct that represents any chessPiece from pawn to king
+}
