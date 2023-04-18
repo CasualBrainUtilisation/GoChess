bf93c507b2538c00b4073826ddcd522b18be4191
CasualBrainUtilisation <120629434+CasualBrainUtilisation@users.noreply.github.com>
2023-04-18 10:25:45 +0100

    can show pieces of both colors

--- Board/Board.go
+++ Board/Board.go
@@ -7,13 +7,24 @@ import (
 )
 
 // Now following all the chessPiece characters, again sorted so the Piece.PieceType can be used as index, those characters are official Unicode characters
-var whiteUnicodeChessPieces [6]string = [6]string{
+// black chess symbols
+var blackUnicodeChessPieces [6]string = [6]string{
 	"♙",
 	"♘",
 	"♗",
 	"♖",
 	"♕",
 	"♔",
+}
+
+// white chess symbols
+var whiteUnicodeChessPieces [6]string = [6]string{
+	"♟︎",
+	"♞",
+	"♝",
+	"♜",
+	"♛",
+	"♚",
 }
 
 func VisualisePositionFromPieces(position []Pieces.Piece) { //shows the current position on the chessBoard in the terminal utilising the chessPiece unicoode characters
@@ -28,7 +39,12 @@ func VisualisePositionFromPieces(position []Pieces.Piece) { //shows the current 
 
 	//ovverride certain empty fields in the visualisation array with the corresponding piece character
 	for _, piece := range position {
-		visualisation[piece.BoardPosition.Y][piece.BoardPosition.X] = whiteUnicodeChessPieces[piece.PieceType]
+		var pieceCharacter string = blackUnicodeChessPieces[piece.PieceType] //set the piece character, that'll use to visualize the piece on the chessBoard, r n we set it to the black piece character of corresponding type
+		if piece.PieceColor == Pieces.White {                                //check if the piece is colored in white, if so set the pieceCharacter to the white counterPart
+			pieceCharacter = whiteUnicodeChessPieces[piece.PieceType]
+		}
+
+		visualisation[piece.BoardPosition.Y][piece.BoardPosition.X] = pieceCharacter //ovveride the character in the visualized board grid at the piece position with the evaluated pieceCharacter
 	}
 
 	//print out the board, each row will be printed in a new line
