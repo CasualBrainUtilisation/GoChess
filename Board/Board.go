5cdae5ffdfcb18e66f7b98543967401a80d2a7d5
CasualBrainUtilisation <120629434+CasualBrainUtilisation@users.noreply.github.com>
2023-04-18 08:29:55 +0100

    chessBoard piece visualisation

--- Board/Board.go
+++ Board/Board.go
@@ -6,14 +6,32 @@ import (
 	"github.com/CasualBrainUtilisation/GoChess/Pieces"
 )
 
+// Now following all the chessPiece characters, again sorted so the Piece.PieceType can be used as index, those characters are official Unicode characters
+var whiteUnicodeChessPieces [6]string = [6]string{
+	"♙",
+	"♘",
+	"♗",
+	"♖",
+	"♕",
+	"♔",
+}
+
 func VisualisePositionFromPieces(position []Pieces.Piece) { //shows the current position on the chessBoard in the terminal utilising the chessPiece unicoode characters
 
-	var visualisation [8][8]string = [8][8]string{{" ", " ", " ", " ", " ", " ", " ", " "}, {" ", " ", " ", " ", " ", " ", " ", " "}, {" ", " ", " ", " ", " ", " ", " ", " "}, {" ", " ", " ", " ", " ", " ", " ", " "}, {" ", " ", " ", " ", " ", " ", " ", " "}, {" ", " ", " ", " ", " ", " ", " ", " "}, {" ", " ", " ", " ", " ", " ", " ", " "}, {" ", " ", " ", " ", " ", " ", " ", " "}}
+	var visualisation [8][8]string //create the array that'll later be printed out to visualize the board
 
-	for _, piece := range position {
-		visualisation[piece.BoardPosition.Y][piece.BoardPosition.X] = "P"
+	for y := 0; y < 8; y++ { //add the empty fields, to the visualisation chessBoard array
+		for x := 0; x < 8; x++ {
+			visualisation[y][x] = "_"
+		}
 	}
 
+	//ovverride certain empty fields in the visualisation array with the corresponding piece character
+	for _, piece := range position {
+		visualisation[piece.BoardPosition.Y][piece.BoardPosition.X] = whiteUnicodeChessPieces[piece.PieceType]
+	}
+
+	//print out the board, each row will be printed in a new line
 	for _, row := range visualisation {
 		fmt.Println(row)
 	}
