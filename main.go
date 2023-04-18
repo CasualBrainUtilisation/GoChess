f9de9f3d0f02d7a22509a1fe415fb5cb1292b9f8
CasualBrainUtilisation <120629434+CasualBrainUtilisation@users.noreply.github.com>
2023-04-18 10:38:11 +0100

    removed old print statements

--- main.go
+++ main.go
@@ -1,23 +1,14 @@
 package main //this is the main file, meaning it'll handle all the other packages, which theirself will handle most chess game logic
 
 import (
-	"fmt"
-
 	"github.com/CasualBrainUtilisation/GoChess/Board"
 	"github.com/CasualBrainUtilisation/GoChess/FEN"
-	"github.com/CasualBrainUtilisation/GoChess/Fields"
 	"github.com/CasualBrainUtilisation/GoChess/Pieces"
 )
 
 func main() {
-	var piece Pieces.Piece = Pieces.Piece{PieceType: Pieces.Bishop, PieceColor: Pieces.Black, BoardPosition: Fields.BoardField{X: 1, Y: 2}}
-
-	fmt.Println(piece)
 
 	var pieces []Pieces.Piece = FEN.LoadPositionFromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-
-	fmt.Println(pieces)
-	fmt.Println("♔", "♚")
 
 	Board.VisualisePositionFromPieces(pieces)
 }
