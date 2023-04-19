61e76d8e33446042fb48e689f3faa694beb8949a
CasualBrainUtilisation <120629434+CasualBrainUtilisation@users.noreply.github.com>
2023-04-19 06:42:41 +0100

    TryToGetMove calculates move.endPos

--- Moves/Moves.go
+++ Moves/Moves.go
@@ -1,8 +1,9 @@
 package Moves
 
 import (
-	"fmt"
+	"strconv"
 	"strings"
+	"unicode"
 
 	"github.com/CasualBrainUtilisation/GoChess/Fields"
 	"github.com/CasualBrainUtilisation/GoChess/Pieces"
@@ -37,11 +38,26 @@ type Move struct { //a stuct to represent a move on the chessBoard, it is used t
 	moveType MoveType //the type of moved performed, e.g.: Normal/Castle ect., needed to perform a move properly or to calculate things like en Passant moves
 }
 
-func TryToPerformMove(move string, boardPosition []Pieces.Piece) { //this function will try to perform move from given move notation, if possible
+func TryToGetMove(moveNotation string, boardPosition []Pieces.Piece) (move Move, ok bool) { //this function will try to get move from given move notation, if it doesn't seem to get a valid move out of it, it'll return false as ok
+
+	move = Move{} //this variable will later be returned, as the move represented by the moveNotation, its variables will be calculated throughout this function
 
 	var curIndex int = 0
 
-	if index := strings.Index(strings.Join(columnLetters[:], ""), strings.ToLower(string(move[curIndex]))); index != -1 { //get the index of the move string as lowercase (as the columnLetters are lowerCase and r n we just check wether the moveString/char represents a column letter) in the columnLetters (converted to a slice, so we can join it to one string) array as joined string, if it does exist (it'll be -1 if it doesn't) continue, adding the x.pos at corresponding position
-		fmt.Println(index)
+	//calculating the endPos of the move
+
+	if index := strings.Index(strings.Join(columnLetters[:], ""), strings.ToLower(string(moveNotation[curIndex]))); index != -1 { //get the index of the move string as lowercase (as the columnLetters are lowerCase and r n we just check wether the moveString/char represents a column letter) in the columnLetters (converted to a slice, so we can join it to one string) array as joined string, if it does exist (it'll be -1 if it doesn't) continue, adding the x.pos at corresponding position
+		move.endPos.X = index //set the x position of the move endPos to the index of the first letter in the letters, as that is what it represents
 	}
+	if unicode.IsNumber(rune(moveNotation[1])) { //check wether the 2nd character of moveNotation, is a number, if so set the move.endPos.Y to it, as that is what it represents
+		endY, err := strconv.Atoi(string(moveNotation[1])) //convert the string to a number
+		if err != nil {                                    //if for some odd reason the string could not be converted to a number (which I could not understand whatshowever) return set ok to false
+			ok = false
+		} else { //set the move.endPos.Y accordingly if the string could be converted
+			move.endPos.Y = endY
+		}
+
+	}
+
+	return //naked return statement, cuz i'm lazy, returns move and ok
 }
