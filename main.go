3009b8c0316d357ef34128a810e48b7e5f50a678
CasualBrainUtilisation <120629434+CasualBrainUtilisation@users.noreply.github.com>
2023-04-19 06:33:30 +0100

    removed test functions

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main //this is the main file, meaning it'll handle all the other package
 import (
 	"github.com/CasualBrainUtilisation/GoChess/Board"
 	"github.com/CasualBrainUtilisation/GoChess/FEN"
-	"github.com/CasualBrainUtilisation/GoChess/Moves"
 	"github.com/CasualBrainUtilisation/GoChess/Pieces"
 )
 
@@ -12,9 +11,4 @@ func main() {
 	var pieces []Pieces.Piece = FEN.LoadPositionFromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
 
 	Board.VisualisePositionFromPieces(pieces)
-
-	Moves.TryToPerformMove("Na6", []Pieces.Piece{})
-	Moves.TryToPerformMove("a6", []Pieces.Piece{})
-	Moves.TryToPerformMove("6", []Pieces.Piece{})
-	Moves.TryToPerformMove("e5", []Pieces.Piece{})
 }
