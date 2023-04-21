ee457f5b1fe42876b670ee19e48f769f4297bbc8
CasualBrainUtilisation <120629434+CasualBrainUtilisation@users.noreply.github.com>
2023-04-21 10:48:22 +0100

    refactored comment

--- Fields/Fields.go
+++ Fields/Fields.go
@@ -4,6 +4,6 @@ type BoardField struct { //struct representing any field on the chessBoard, used
 	X, Y int
 }
 
-func IsFieldOnBoard(field BoardField) bool { //checks wether given BoardField even is on a chessBoard (aka x and y are between 8 and 0)
+func IsFieldOnBoard(field BoardField) bool { //checks wether given BoardField position even is on a chessBoard (aka x and y are between 8 and 0)
 	return field.X >= 0 && field.X < 8 && field.Y >= 0 && field.Y < 8
 }
