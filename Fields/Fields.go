16a9423b833e455953e98d0a7906801151974d04
CasualBrainUtilisation <120629434+CasualBrainUtilisation@users.noreply.github.com>
2023-04-21 10:45:53 +0100

    added IsFieldOnBoard

--- Fields/Fields.go
+++ Fields/Fields.go
@@ -3,3 +3,7 @@ package Fields
 type BoardField struct { //struct representing any field on the chessBoard, used primarly to represent a piece's position
 	X, Y int
 }
+
+func IsFieldOnBoard(field BoardField) bool { //checks wether given BoardField even is on a chessBoard (aka x and y are between 8 and 0)
+	return field.X >= 0 && field.X < 8 && field.Y >= 0 && field.Y < 8
+}
