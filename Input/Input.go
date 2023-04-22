b1189740364d40ee781b2b483f3d44938116fa86
CasualBrainUtilisation <120629434+CasualBrainUtilisation@users.noreply.github.com>
2023-04-22 08:28:02 +0100

    added Input.go

---
+++ Input/Input.go
@@ -0,0 +1,5 @@
+package Input //package that manages the user input from the terminal an calls corresponding functions from other packages e. g. = game load "" / Na4 ect.
+
+func getInput() { //loop that'll constantly run, it'll wait for user input, after doing what the user comanded it'll wait for new input ect
+
+}
