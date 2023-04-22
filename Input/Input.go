028c49843851f5b349c0e763d2253a0e25b980c0
CasualBrainUtilisation <120629434+CasualBrainUtilisation@users.noreply.github.com>
2023-04-22 15:56:51 +0100

    added game load

--- Input/Input.go
+++ Input/Input.go
@@ -24,7 +24,7 @@ func RespondToUserInputRepeatly(chessBoard Board.ChessBoard) { //loop that'll co
 			fmt.Println("Ending the game")
 			return
 		case "game": //game is used to start new games and load FENs ect. for that we'll search for further subcommands
-			if len(commandParts) == 0 { //if there is no subcommand, give the user a brief description of the game command
+			if len(commandParts) == 1 { //if there is no subcommand, give the user a brief description of the game command
 				fmt.Println("game is used to start a new game or load a position from a FEN")
 			} else {
 				switch strings.ToLower(commandParts[1]) { //search for valid subcommands
@@ -32,8 +32,15 @@ func RespondToUserInputRepeatly(chessBoard Board.ChessBoard) { //loop that'll co
 					fmt.Println("starting a new game") //inform the user that its command'll be executed
 					fmt.Println()
 
-					var chessBoard = Board.ChessBoard{CurPieces: FEN.LoadPositionFromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")} //create a chessBoard with the start position loaded from the start FEN
-					chessBoard.VisualisePositionFromPieces()                                                                                          //print out chessBoard
+					chessBoard = Board.ChessBoard{CurPieces: FEN.LoadPositionFromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")} //create a chessBoard with the start position loaded from the start FEN
+					chessBoard.VisualisePositionFromPieces()                                                                                      //print out chessBoard
+				case "load": //this will load the FEN in the commandPart[2]
+					if len(commandParts) == 2 { //if there is no subcommand to load, give the user a brief description of the game load command
+						fmt.Println("load can be used to load a FEN on the CLI chessBoard")
+					} else {
+						chessBoard = Board.ChessBoard{CurPieces: FEN.LoadPositionFromFEN(commandParts[2])} //load the FEN on a new chessBoard, we'll store in the chessBoard variable
+						chessBoard.VisualisePositionFromPieces()                                           //print out loaded chessBoard
+					}
 				default:
 					fmt.Println("invalid subcommand for 'game'")
 				}
