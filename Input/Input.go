e9d166cef9530e4fde0e5db789f014dd513d5ea5
CasualBrainUtilisation <120629434+CasualBrainUtilisation@users.noreply.github.com>
2023-04-23 07:14:36 +0100

    fixed some input issues

--- Input/Input.go
+++ Input/Input.go
@@ -20,39 +20,44 @@ func RespondToUserInputRepeatly(chessBoard Board.ChessBoard) { //loop that'll co
 
 		var commandParts []string = strings.Fields(input) //get the parts of the user's input, divided by whitespaces, so we can access subcommands easily
 
-		switch strings.ToLower(commandParts[0]) { //check if the user inputted certain things and if so run the corresponding functions (compare lowercased so the case doesn't matter)
-		case "quit", "stop", "exit": //in case the user put in 'quit' ect, we'll end this programm by ending the function with return
-			fmt.Println()
-			fmt.Println("Ending the game")
-			return
-		case "game": //game is used to start new games and load FENs ect. for that we'll search for further subcommands
-			if len(commandParts) == 1 { //if there is no subcommand, give the user a brief description of the game command
-				fmt.Println("game is used to start a new game or load a position from a FEN")
-			} else {
-				switch strings.ToLower(commandParts[1]) { //search for valid subcommands
-				case "new":
-					fmt.Println("starting a new game") //inform the user that its command'll be executed
-					fmt.Println()
+		if len(commandParts) > 0 { //only if the user even inputted something besides the newLine, so there is a commandPart '0', we'll check what it is
+			switch strings.ToLower(commandParts[0]) { //check if the user inputted certain things and if so run the corresponding functions (compare lowercased so the case doesn't matter)
+			case "quit", "stop", "exit": //in case the user put in 'quit' ect, we'll end this programm by ending the function with return
+				fmt.Println()
+				fmt.Println("Ending the game")
+				return
+			case "game": //game is used to start new games and load FENs ect. for that we'll search for further subcommands
+				if len(commandParts) == 1 { //if there is no subcommand, give the user a brief description of the game command
+					fmt.Println("game is used to start a new game or load a position from a FEN")
+				} else {
+					switch strings.ToLower(commandParts[1]) { //search for valid subcommands
+					case "new":
+						fmt.Println("starting a new game") //inform the user that its command'll be executed
+						fmt.Println()
 
-					chessBoard = Board.ChessBoard{CurPieces: FEN.LoadPositionFromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")} //create a chessBoard with the start position loaded from the start FEN
-					chessBoard.VisualisePositionFromPieces()                                                                                      //print out chessBoard
-				case "load": //this will load the FEN in the commandPart[2]
-					if len(commandParts) == 2 { //if there is no subcommand to load, give the user a brief description of the game load command
-						fmt.Println("load can be used to load a FEN on the CLI chessBoard")
-					} else {
-						chessBoard = Board.ChessBoard{CurPieces: FEN.LoadPositionFromFEN(commandParts[2])} //load the FEN on a new chessBoard, we'll store in the chessBoard variable
-						chessBoard.VisualisePositionFromPieces()                                           //print out loaded chessBoard
+						chessBoard = Board.ChessBoard{CurPieces: FEN.LoadPositionFromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")} //create a chessBoard with the start position loaded from the start FEN
+						chessBoard.VisualisePositionFromPieces()                                                                                      //print out chessBoard
+					case "load": //this will load the FEN in the commandPart[2]
+						if len(commandParts) == 2 { //if there is no subcommand to load, give the user a brief description of the game load command
+							fmt.Println("load can be used to load a FEN on the CLI chessBoard")
+						} else {
+							chessBoard = Board.ChessBoard{CurPieces: FEN.LoadPositionFromFEN(commandParts[2])} //load the FEN on a new chessBoard, we'll store in the chessBoard variable
+							chessBoard.VisualisePositionFromPieces()                                           //print out loaded chessBoard
+						}
+					default:
+						fmt.Println("invalid subcommand for 'game'")
 					}
-				default:
-					fmt.Println("invalid subcommand for 'game'")
+				}
+			default: //if no specific command was inputted it should be a sinple chessMove which we'll try to get and perform
+				if move, ok := MoveNotation.TryToGetMoveFromNotation(chessBoard, commandParts[0]); ok == true { //try to get the move the user inputted, if a move was inputted, perform it
+					Moves.PerformMove(chessBoard, move)
+					chessBoard.VisualisePositionFromPieces() //print out chessBoard with moved piece
+				} else {
+					fmt.Println("invalid command")
 				}
 			}
-		default: //if no specific command was inputted it should be a sinple chessMove which we'll try to get and perform
-			if move, ok := MoveNotation.TryToGetMoveFromNotation(chessBoard, commandParts[0]); ok == true { //try to get the move the user inputted, if a move was inputted, perform it
-				Moves.PerformMove(chessBoard, move)
-				chessBoard.VisualisePositionFromPieces() //print out chessBoard with moved piece
-			}
 		}
+
 		fmt.Println() //always print a line here to seperate the user input line from the new output
 	}
 }
